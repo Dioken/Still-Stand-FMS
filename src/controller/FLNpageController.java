ab467277cefe3d2b0f222ddec0afdaf98eb24f8a
Oumar sow <sowoumar25@yahoo.fr>
2016-04-22 14:06:09 +0200

    Rajout du clavier

--- src/controller/FLNpageController.java
+++ src/controller/FLNpageController.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import main.StillStandFMS;
@@ -31,6 +32,26 @@ public class FLNpageController {
             loaderCenter.setLocation(StillStandFMS.class.getResource("/view/Liste.fxml"));
             AnchorPane FXMLViewCenter = (AnchorPane) loaderCenter.load();
             rootBorder.setCenter(FXMLViewCenter);
+            
+            //A mettre dans un autre controlleur ce qui est en commentaire
+            /* 
+            FXMLLoader loaderCenter = new FXMLLoader();
+            loaderCenter.setLocation(StillStandFMS.class.getResource("/view/Clavier.fxml"));
+            AnchorPane FXMLViewCenter = (AnchorPane) loaderCenter.load();
+            SplitPane splitPane = (SplitPane) FXMLViewCenter.getChildren().get(0);
+            
+            FXMLLoader loaderDown = new FXMLLoader();
+            loaderDown.setLocation(StillStandFMS.class.getResource("/view/Clavier.fxml"));
+            AnchorPane clavier = (AnchorPane) loaderDown.load();
+                    
+            
+            splitPane.getItems().set(1, clavier);
+            rootBorder.setCenter(FXMLViewCenter);*/
+            
+            //FXMLLoader loaderDown = new FXMLLoader();
+            //loaderDown.setLocation(StillStandFMS.class.getResource("/view/Clavier.fxml"));
+            //rootBorder.getChildren().get(0).setClip(FXMLLoader);
+            //FXMLViewCenter.getChildren().add(loaderDown);
         } catch (IOException ex) {
             Logger.getLogger(MenuOverviewController.class.getName()).log(Level.SEVERE, null, ex);
         }
