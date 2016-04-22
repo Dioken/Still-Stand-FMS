3c67d61b2ebbccc2a9fda9f03254ac951b85af4c
YANLINQU <quyanlingg@hotmail.com>
2016-04-22 16:53:57 +0200

    ajouter splitPane pour FLNpage et clavier

--- src/controller/FLNpageController.java
+++ src/controller/FLNpageController.java
@@ -5,12 +5,15 @@
  */
 package controller;
 
+import been.ListChemin;
+import java.awt.Button;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import main.StillStandFMS;
@@ -21,9 +24,13 @@ import main.StillStandFMS;
  */
 public class FLNpageController {
     private BorderPane rootBorder;
+    private SplitPane rootSplit;
+    private SplitPane splitPane;
     
-    public void setFLNpageController(BorderPane rootBorder){
+    
+    public void setFLNpageController(BorderPane rootBorder,SplitPane rootSplit){
         this.rootBorder = rootBorder;
+        this.rootSplit = rootSplit;
     }
     @FXML
     public void handleEntererListe(){
@@ -32,9 +39,21 @@ public class FLNpageController {
             loaderCenter.setLocation(StillStandFMS.class.getResource("/view/Liste.fxml"));
             AnchorPane FXMLViewCenter = (AnchorPane) loaderCenter.load();
             rootBorder.setCenter(FXMLViewCenter);
-
+            
+            //charger listCheminController
             ListCheminController controller = loaderCenter.getController();
-            controller.setList();
+            /*
+            ListChemin: 4 attributs
+            from: depart
+            dest: arrive
+            dist: distance
+            radio: lu numero de radio
+            */
+            controller.setList("from1","dest1","dist1","radio1");
+            controller.setList("from2","dest2","dist2","radio2");
+            controller.setList("from3","dest3","dist3","radio3");
+            // afficher des donnee sur la TableView
+            controller.afficherList();
 
             
             //A mettre dans un autre controlleur ce qui est en commentaire
@@ -60,5 +79,32 @@ public class FLNpageController {
             Logger.getLogger(MenuOverviewController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    @FXML
+    public void handleAfficherLeCheminEnCourant(){
+        try {
+            /*
+            charger la page clavier sur le split en bas
+            */
+            FXMLLoader loaderClavier = new FXMLLoader();
+            loaderClavier.setLocation(StillStandFMS.class.getResource("../view/Clavier.fxml"));                            
+            AnchorPane FXMLViewClavier;
+            FXMLViewClavier = (AnchorPane) loaderClavier.load();
+            rootSplit.getItems().set(1, FXMLViewClavier);
+        } catch (IOException ex) {
+            Logger.getLogger(FLNpageController.class.getName()).log(Level.SEVERE, null, ex);
+        }            
+    }     
     
+    @FXML
+    public void apuyerBtValide(){        
+        try {
+            FXMLLoader loaderClavier = new FXMLLoader();
+            loaderClavier.setLocation(StillStandFMS.class.getResource("../view/Liste.fxml"));
+            AnchorPane FXMLViewClavier = (AnchorPane) loaderClavier.load();
+            rootSplit.getItems().set(1, FXMLViewClavier);
+        } catch (IOException ex) {
+            Logger.getLogger(FLNpageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 }
--- src/controller/GoAroundController.java
+++ src/controller/GoAroundController.java
@@ -6,10 +6,12 @@
 package controller;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import main.StillStandFMS;
@@ -20,7 +22,10 @@ import main.StillStandFMS;
  */
 public class GoAroundController {    
     private BorderPane rootBorder;
+    private SplitPane rootSplit;
     FXMLLoader loaderCenter = null;
+    FXMLLoader loaderSplit = null;
+    FXMLLoader loaderClavier = null;
     
     public void setGoAround(BorderPane rootBorder){
         this.rootBorder = rootBorder;
@@ -30,9 +35,19 @@ public class GoAroundController {
     public void handleEntererFLNPage() {
         try {                    
             loaderCenter = new FXMLLoader();
-            loaderCenter.setLocation(StillStandFMS.class.getResource("../view/FLNpage.fxml"));
-            AnchorPane FXMLViewCenter = (AnchorPane) loaderCenter.load();
-            rootBorder.setCenter(FXMLViewCenter);
+            loaderCenter.setLocation(StillStandFMS.class.getResource("../view/FLNpageSplit.fxml"));
+            rootSplit = (SplitPane) loaderCenter.load();
+            rootBorder.setCenter(rootSplit);
+            
+            /*
+            Charger la page FLN sur le split  en haut.
+            */
+            loaderSplit = new FXMLLoader();
+            loaderSplit.setLocation(StillStandFMS.class.getResource("../view/FLNpage.fxml"));            
+            AnchorPane FXMLViewSplit = (AnchorPane) loaderSplit.load();
+            rootSplit.getItems().set(0, FXMLViewSplit);
+            
+            
         } catch (IOException ex) {
             Logger.getLogger(MenuOverviewController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -41,8 +56,8 @@ public class GoAroundController {
     }
     
     public void chargerFLNController(){
-        FLNpageController flnController = loaderCenter.getController();
-        flnController.setFLNpageController(rootBorder);  
+        FLNpageController flnController = loaderSplit.getController();
+        flnController.setFLNpageController(rootBorder,rootSplit);  
     }
     
 }
